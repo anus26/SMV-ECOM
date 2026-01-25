4d882e53924c8f2f08c485421d7eba0f924b7204
anus26 <anusraza268@gmail.com>
2026-01-25 09:19:37 -0800

    order work

--- src/controllers/order.contollers.js
+++ src/controllers/order.contollers.js
@@ -22,4 +22,62 @@ const order=async(req,res)=>{
         
     }
 }
-export {order}
+const orderdelete=async(req,res)=>{
+    try {
+        const {id}=req.params
+        const order=await Order.findByIdAndDelete(id)
+        if (!order) {
+            return res.status(400).json({message:'Order not',order})
+        }
+        res.status(200).json({message:"Order Delete"})
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({message:"order delete"})
+        
+    }
+}
+const orderget=async(req,res)=>{
+    try {
+        const {id}=req.params
+        const ordergets=await Order.findById(id)
+        if (!ordergets) {
+                 return res.status(400).json({message:'Order not'})
+        }
+        res.status(200).json({message:'order get successfully',ordergets})
+    } catch (error) {
+        console.error('error',error);
+        return res.status(500).json({message:"Internal server error"})
+        
+    }
+}
+const orderupdata=async(req,res)=>{
+ try {
+      const {id}=req.params
+      const updataFileds={}
+      if (req.body.items) updataFileds.items=req.body.items
+          if (req.body.status) updataFileds.status=req.body.status
+              if (req.body.totalAmount) updataFileds.totalAmount=req.body.totalAmount
+      const orderupdatas=await Order.findByIdAndUpdate(id,{$set:updataFileds},{new:true,runValidators:true})
+      if (!order) {
+           return res.status(400).json({message:'Order not'})
+      } 
+      res.status(201).json({message:'Order updata',orderupdatas})
+ } catch (error) {
+       console.error('error',error);
+        return res.status(500).json({message:"Internal server error"})
+ }
+
+}
+const allorder=async(req,res)=>{
+    try {
+        const allorders=await Order.find()
+        if (!allorders) {
+            return res.status(400).json({message:"Order are not avalibliy"})
+        }
+        res.status(200).json({message:"All  order is availbaly" ,allorders})
+    } catch (error) { 
+         console.error('error',error);
+        return res.status(500).json({message:"Internal server error"})
+    }
+}
+export {order,orderdelete,orderget,orderupdata,allorder}
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,6 +1,10 @@
 import  express  from "express";
-import { order } from '../controllers/order.contollers.js'
-import { authmiddleware } from "../middleware/user.middleware.js";
+import { allorder, order, orderdelete, orderget, orderupdata } from '../controllers/order.contollers.js'
+import { authmiddleware, authorizationRole } from "../middleware/user.middleware.js";
 const orderrouter=express.Router()
-orderrouter.post("/orderadd",authmiddleware,order)
+orderrouter.post("/orderadd",authmiddleware,authorizationRole('customer'),order)
+orderrouter.get("/orderget/:id",authmiddleware,orderget)
+orderrouter.get("/allorder",authmiddleware,authorizationRole("seller"),allorder)
+orderrouter.put("/updateorder/:id",orderupdata)
+orderrouter.delete("/orderdelete/:id",orderdelete)
 export default orderrouter
