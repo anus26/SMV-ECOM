dc743cd44b7c2ae5f0f421776940ebc9023c9f7d
anus26 <anusraza268@gmail.com>
2026-02-19 05:28:17 -0800

    status

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://frontend-smv-ecom-b43cybf3x-anusrazas-projects.vercel.app",
   credentials:true
 }))
 app.use(cookieParser())
