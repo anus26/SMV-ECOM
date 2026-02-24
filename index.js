8d7e1720bda741b9daf9faf1dcac04bb1a1000c3
anus26 <anusraza268@gmail.com>
2026-02-23 21:15:22 -0800

    modif

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin:"https://frontend-smv-ecom.vercel.app",
-  // origin:'http://localhost:5173',
+
   credentials:true
 }))
 app.use(cookieParser())
