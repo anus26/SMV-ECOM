0a7cc06282f74fddd0ecae2a5ce8ab91152ca98a
anus26 <anusraza268@gmail.com>
2026-03-06 14:35:46 -0800

    mod

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use("/api/v1/webhook",webhookrouter)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  // origin:"https://frontend-smv-ecom.vercel.app",
-  origin:"http://localhost:5173",
+  origin:"https://frontend-smv-ecom.vercel.app",
+  // origin:"http://localhost:5173",
   credentials:true
 }))
 app.use(cookieParser())
